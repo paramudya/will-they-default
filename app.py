0e4cf789911394ed363fddb136bb0e2297b12797
paramudya <67263982+paramudya@users.noreply.github.com>
2021-09-20 09:08:03 +0700

    Add files via upload
    render template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, json, request
+from flask import Flask, jsonify, json, request, render_template
 from logging import debug
 import pickle
 from flask_cors import CORS # library for handling cross origin resources sharing.
