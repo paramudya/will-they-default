3f04cc7b7a90a5b84293fd996c65c6bc1b9feb7d
paramudya <67263982+paramudya@users.noreply.github.com>
2021-09-20 18:20:40 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,30 +5,39 @@ from flask_cors import CORS # library for handling cross origin resources sharin
 from predict import predict_data
 
 '''
-Ini app.py yang untuk dicoba ke POSTMAN. Ga dikasih wrapper.
+Ini script untuk dicoba ke POSTMAN. Ga dikasih wrapper.
 '''
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/predict", methods=['POST'])
-def do_predict():
+def predict():
     if request.method == 'POST':
         data_input = request.get_json()
-        print('Input data:\n',data_input,'\nwith type:',type(data_input))
-        result = predict_data(data_input)
-        
+
+        data["person_age"] = data_input.get("person_age")
+        data["person_income"] = data_input.get("person_income")
+        data["person_home_ownership"] = data_input.get("person_home_ownership")
+        data["person_emp_length"] = data_input.get("person_emp_length")
+        data["loan_intent accounts"] = data_input.get("loan_intent")
+        data["loan_grade"] = data_input.get("loan_grade")
+        data["loan_amnt"] = data_input.get("loan_amnt")
+        data["loan_percent_income"] = data_input.get("loan_percent_income")
+        data["cb_person_default_on_file"] = data_input.get("cb_person_default_on_file")
+        data["cb_person_cred_hist_length"] = data_input.get("cb_person_cred_hist_length")
+
+        result = predict_data(data)
+
         threshold = 0.5
         risk = True if result >= threshold else False
-        
+
         result_dict = {
             'model':'credit-risk-scorer-dari-modul-agustiar',
             'risky':risk,
             'score_proba':float(result[0]),
             'version': '1.0.Ordinal-punya-sendiri',
-
         }
-        print('Proba of risk: ',result)
     return jsonify(result_dict)
 
 if __name__ == '__main__':
