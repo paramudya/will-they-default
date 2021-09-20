7b143a4a8e17a88f13655c715f662354d9b160d6
paramudya <67263982+paramudya@users.noreply.github.com>
2021-09-20 18:18:55 +0700

    Update app.py
    default and get value dict (force nan instead of error)

--- app.py
+++ app.py
@@ -9,15 +9,26 @@ def new_app():
     CORS(app)
 
     @app.route("/", methods=['GET'])
-    def hello():
+    def default():
         return render_template("index.html")
 
     @app.route("/predict", methods=['POST'])
     def predict():
         if request.method == 'POST':
             data_input = request.get_json()
-            print('Input data:\n',data_input,'\nwith type:',type(data_input))
-            result = predict_data(data_input)
+
+            data["person_age"] = data_input.get("person_age")
+            data["person_income"] = data_input.get("person_income")
+            data["person_home_ownership"] = data_input.get("person_home_ownership")
+            data["person_emp_length"] = data_input.get("person_emp_length")
+            data["loan_intent accounts"] = data_input.get("loan_intent")
+            data["loan_grade"] = data_input.get("loan_grade")
+            data["loan_amnt"] = data_input.get("loan_amnt")
+            data["loan_percent_income"] = data_input.get("loan_percent_income")
+            data["cb_person_default_on_file"] = data_input.get("cb_person_default_on_file")
+            data["cb_person_cred_hist_length"] = data_input.get("cb_person_cred_hist_length")
+            
+            result = predict_data(data)
             
             threshold = 0.5
             risk = True if result >= threshold else False
@@ -27,9 +38,7 @@ def new_app():
                 'risky':risk,
                 'score_proba':float(result[0]),
                 'version': '1.0.Ordinal-punya-sendiri',
-
             }
-            print('Proba of risk: ',result)
         return jsonify(result_dict)
     return app
 
