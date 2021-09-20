649a57367495f7eb7231e711b90ccd2c0459600f
paramudya <67263982+paramudya@users.noreply.github.com>
2021-09-21 00:16:54 +0700

    DIBENERIN BUAT AMBI LSAVED MODELS TRS JD JALAN LAGI
     (SBLMNYA JG JALAN SIH)

--- predict.py
+++ predict.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import pickle
-# from collections import defaultdict
 import numpy as np
 import xgboost
 
@@ -20,26 +19,25 @@ raw_input={'person_age': 30,
 #   'loan_status_proba': 0.013454122468829155
   }
 
-with open('saved_models/pipe_to_deploy.pkl', 'rb') as f_in: 
-    preprocess= pickle.load(f_in)
+def predict_data(raw_input):
+    with open('saved_models/pipe_to_deploy.pkl', 'rb') as f_in: 
+        preprocess= pickle.load(f_in)
 
-with open('saved_models/xgb_to_deploy.pkl', 'rb') as f: 
-    model= pickle.load(f)
-    
-def formating_data(raw_input):
-    raw_input = pd.DataFrame.from_dict(raw_input, orient='index').T.replace({
+    with open('saved_models/xgb_to_deploy.pkl', 'rb') as f: 
+        model= pickle.load(f)
+
+    raw_processed = pd.DataFrame.from_dict(raw_input, orient='index').T.replace({
         None: np.nan,
         "null":np.nan,
         "" : np.nan
     })
-    return raw_input
-    
-def preprocess_data(raw_input):
-    X = preprocess.transform(raw_input)
-    return X
-
-
-def predict_data(data):
-    result = model.predict_proba(preprocess_data(formating_data(data)))[:,1]
+    X = preprocess.transform(raw_processed)
+    result = model.predict_proba(X)[:,1]
     return result
-   
+    
+if __name__ == "__main__":
+    predicted=predict_data(raw_input)
+
+    print(type(predicted))
+    print(predicted)
+    # assert score_proba == result
--- test_app.py
+++ test_app.py
@@ -15,14 +15,15 @@ CORS(app)
 def predict():
     if request.method == 'POST':
         data_input = request.get_json()
-
+        data={}
         data["person_age"] = data_input.get("person_age")
         data["person_income"] = data_input.get("person_income")
         data["person_home_ownership"] = data_input.get("person_home_ownership")
         data["person_emp_length"] = data_input.get("person_emp_length")
-        data["loan_intent accounts"] = data_input.get("loan_intent")
+        data["loan_intent"] = data_input.get("loan_intent")
         data["loan_grade"] = data_input.get("loan_grade")
         data["loan_amnt"] = data_input.get("loan_amnt")
+        data["loan_int_rate"] = data_input.get("loan_int_rate")
         data["loan_percent_income"] = data_input.get("loan_percent_income")
         data["cb_person_default_on_file"] = data_input.get("cb_person_default_on_file")
         data["cb_person_cred_hist_length"] = data_input.get("cb_person_cred_hist_length")
@@ -33,10 +34,10 @@ def predict():
         risk = True if result >= threshold else False
 
         result_dict = {
-            'model':'credit-risk-scorer-dari-modul-agustiar',
+            'model':'credit-risk-predictor',
             'risky':risk,
             'score_proba':float(result[0]),
-            'version': '1.0.Ordinal-punya-sendiri',
+            'version': '0.4.6',
         }
     return jsonify(result_dict)
 
@@ -45,7 +46,6 @@ if __name__ == '__main__':
 
 '''
 input format:
-
  {
      "person_age": 26,
   "person_income": 48000,
@@ -61,17 +61,13 @@ input format:
  }
  
 expected test proba result from above input: .03851019963622093
-
-
 expected output format:
 {
                 'model':'credit-risk-scorer-dari-modul-agustiar',
                 'risky':False,
                 'score_proba':.03851019963622093,
                 'version': '1.0.Ordinal-punya-sendiri',
-
             }
-
 http to access from(?):
 http://127.0.0.1:5000/predict
 with method POST
