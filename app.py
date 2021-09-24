d4657920143ef147dacd96eefc6712844a5c3744
paramudya <67263982+paramudya@users.noreply.github.com>
2021-09-24 07:50:13 +0700

    hehe online online kl kata saykoji

--- app.py
+++ app.py
@@ -4,41 +4,42 @@ import pickle
 from flask_cors import CORS # library for handling cross origin resources sharing.
 from predict import predict_data
 
-def new_app():
-    app = Flask(__name__)
-    CORS(app)
+app = Flask(__name__)
+CORS(app)
 
-#     @app.route("/", methods=["GET"])
-#     def default():
-#         return render_template("index.html") #kurang ini?
+@app.route("/", methods=["GET"])
+def default():
+    return render_template("index.html") #kurang ini?
 
-    @app.route("/predict", methods=['POST'])
-    def predict():
-        if request.method == 'POST':
-            data_input = request.get_json()
-            data={}
-            data["person_age"] = data_input.get("person_age")
-            data["person_income"] = data_input.get("person_income")
-            data["person_home_ownership"] = data_input.get("person_home_ownership")
-            data["person_emp_length"] = data_input.get("person_emp_length")
-            data["loan_intent"] = data_input.get("loan_intent")
-            data["loan_grade"] = data_input.get("loan_grade")
-            data["loan_amnt"] = data_input.get("loan_amnt")
-            data["loan_int_rate"] = data_input.get("loan_int_rate")
-            data["loan_percent_income"] = data_input.get("loan_percent_income")
-            data["cb_person_default_on_file"] = data_input.get("cb_person_default_on_file")
-            data["cb_person_cred_hist_length"] = data_input.get("cb_person_cred_hist_length")
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        data_input = request.get_json()
+        data={}
+        data["person_age"] = int(data_input['data']['person_age'])
+        data["person_income"] = int(data_input['data']['person_income'])
+        data["person_home_ownership"] = data_input['data']['person_home_ownership']
+        data["person_emp_length"] = float(data_input['data']['person_emp_length'])
+        data["loan_intent"] = data_input['data']['loan_intent']
+        data["loan_grade"] = data_input['data']['loan_grade']
+        data["loan_amnt"] = int(data_input['data']['loan_amnt'])
+        data["loan_int_rate"] = float(data_input['data']['loan_int_rate'])
+        data["loan_percent_income"] = float(data_input['data']['loan_percent_income'])
+        data["cb_person_default_on_file"] = data_input['data']['cb_person_default_on_file']
+        data["cb_person_cred_hist_length"] = int(data_input['data']['cb_person_cred_hist_length'])
+        print('INPUT',data_input)
+        result = predict_data(data)
 
-            result = predict_data(data)
+        threshold = 0.5
+        risk = True if result >= threshold else False
 
-            threshold = 0.5
-            risk = True if result >= threshold else False
+        result_dict = {
+            'model':'credit-risk-predictor',
+            'risky':risk,
+            'score_proba':float(result[0]),
+            'version': '0.4.6',
+        }
+    return jsonify(result_dict)
 
-            result_dict = {
-                'model':'credit-risk-predictor',
-                'risky':risk,
-                'score_proba':float(result[0]),
-                'version': '0.4.6',
-            }
-        return jsonify(result_dict)
-    return app
+if __name__ == '__main__':
+    app.run(port=5000, debug=False)
--- predict.py
+++ predict.py
@@ -3,22 +3,6 @@ import pickle
 import numpy as np
 import xgboost
 
-#format input sekalian buat testing
-raw_input={'person_age': 30,
-  'person_income': 82000,
-  'person_home_ownership': 'MORTGAGE',
-  'person_emp_length': 5.0,
-  'loan_intent': 'PERSONAL',
-  'loan_grade': 'A',
-  'loan_amnt': 5000,
-  'loan_int_rate': 6.76,
-  'loan_percent_income': 0.06,
-  'cb_person_default_on_file': 'N',
-  'cb_person_cred_hist_length': 8,
-#   'loan_status_predict': 0,
-#   'loan_status_proba': 0.013454122468829155
-  }
-
 def predict_data(raw_input):
     with open('saved_models/pipe_to_deploy.pkl', 'rb') as f_in: 
         preprocess= pickle.load(f_in)
@@ -35,9 +19,21 @@ def predict_data(raw_input):
     result = model.predict_proba(X)[:,1]
     return result
     
+    # to test
 if __name__ == "__main__":
+    raw_input={'person_age': 30,
+  'person_income': 82000,
+  'person_home_ownership': 'MORTGAGE',
+  'person_emp_length': 5.0,
+  'loan_intent': 'PERSONAL',
+  'loan_grade': 'A',
+  'loan_amnt': 5000,
+  'loan_int_rate': 6.76,
+  'loan_percent_income': 0.06,
+  'cb_person_default_on_file': 'N',
+  'cb_person_cred_hist_length': 8,
+#   'loan_status_predict': 0,
+#   'loan_status_proba': 0.00918636-ish
+  }
     predicted=predict_data(raw_input)
-
-    print(type(predicted))
     print(predicted)
-    # assert score_proba == result
