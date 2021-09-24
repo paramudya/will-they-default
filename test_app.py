c39ee10417e5ce6333e83c2c1d08e8c8dab0529e
paramudya <67263982+paramudya@users.noreply.github.com>
2021-09-24 07:55:04 +0700

    Add files via upload

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, json, request
+from flask import Flask, jsonify, json, request,render_template
 from logging import debug
 import pickle
 from flask_cors import CORS # library for handling cross origin resources sharing.
@@ -11,22 +11,26 @@ Ini script untuk dicoba ke POSTMAN. Ga dikasih wrapper.
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/", methods=["GET"])
+def default():
+    return render_template("index_test.html") #kurang ini?
+
 @app.route("/predict", methods=['POST'])
 def predict():
     if request.method == 'POST':
         data_input = request.get_json()
         data={}
-        data["person_age"] = data_input.get("person_age")
-        data["person_income"] = data_input.get("person_income")
-        data["person_home_ownership"] = data_input.get("person_home_ownership")
-        data["person_emp_length"] = data_input.get("person_emp_length")
-        data["loan_intent"] = data_input.get("loan_intent")
-        data["loan_grade"] = data_input.get("loan_grade")
-        data["loan_amnt"] = data_input.get("loan_amnt")
-        data["loan_int_rate"] = data_input.get("loan_int_rate")
-        data["loan_percent_income"] = data_input.get("loan_percent_income")
-        data["cb_person_default_on_file"] = data_input.get("cb_person_default_on_file")
-        data["cb_person_cred_hist_length"] = data_input.get("cb_person_cred_hist_length")
+        data["person_age"] = int(data_input['data']['person_age'])
+        data["person_income"] = int(data_input['data']['person_income'])
+        data["person_home_ownership"] = data_input['data']['person_home_ownership']
+        data["person_emp_length"] = float(data_input['data']['person_emp_length'])
+        data["loan_intent"] = data_input['data']['loan_intent']
+        data["loan_grade"] = data_input['data']['loan_grade']
+        data["loan_amnt"] = int(data_input['data']['loan_amnt'])
+        data["loan_int_rate"] = float(data_input['data']['loan_int_rate'])
+        data["loan_percent_income"] = float(data_input['data']['loan_percent_income'])
+        data["cb_person_default_on_file"] = data_input['data']['cb_person_default_on_file']
+        data["cb_person_cred_hist_length"] = int(data_input['data']['cb_person_cred_hist_length'])
 
         result = predict_data(data)
 
